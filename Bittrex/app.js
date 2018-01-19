3fbc67d5d9c2117a44583be0c6f3b863cf528524
“Hassan <hassan.almandeel@gmail.com>
2018-01-18 20:13:36 -0800

    test bittrex api

--- Bittrex/app.js
+++ Bittrex/app.js
@@ -3,6 +3,21 @@ const bittrex = require('node-bittrex-api');
 
 const app = express();
 
+bittrex.options({
+  'apikey' : '',
+  'apisecret' : ''
+});
+
+bittrex.getmarketsummaries( function( data, err ) {
+  if (err) {
+    return console.error(err);
+  }
+  for( var i in data.result ) {
+    bittrex.getticker( { market : data.result[i].MarketName }, function( ticker ) {
+      console.log( ticker );
+    });
+  }
+});
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
